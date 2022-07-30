91e0dbef9c05834724ec339a021787d2e5b37b98
knockerjoker <sean2631@korea.ac.kr>
2022-07-30 17:16:40 +0900

    feat: add scan auto ticket numbers

--- src/view/InputManual.java
+++ src/view/InputManual.java
@@ -1,25 +1,45 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputManual {
     private final static Scanner SCANNER = new Scanner(System.in);
     private final int manualTickets;
+    private final List<String> manualNumberString;
 
-    public InputManual(int manualTickets) {
+    private InputManual(int manualTickets) {
         this.manualTickets = manualTickets;
+        this.manualNumberString = getManualNumberStringInputs(manualTickets);
     }
 
-    private static InputManual enterManualTickets(){
+    public static InputManual enterInputManual(){
         return new InputManual(getInput());
     }
 
-    public static int getInput(){
+    private static int getInput(){
         System.out.println("수동 매수를 입력해주세요: ");
         return SCANNER.nextInt();
     }
 
+    private List<String> getManualNumberStringInputs(final int manualTickets){
+        System.out.println("수동으로 구매할 번호를 입력해주세요");
+        for (int i = 0; i < manualTickets; i++){
+            manualNumberString.add(scanManualNumberString());
+        }
+        return manualNumberString;
+    }
+
     public int getManualTickets(){
         return manualTickets;
+    }
+
+    private static String scanManualNumberString(){
+        return SCANNER.nextLine();
+    }
+
+    public List<String> getManualNumberString(){
+        return manualNumberString;
     }
 }
